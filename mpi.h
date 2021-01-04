03af9816483736fc6ddb79029bd810b1ecfab540
David Barina <ibarina@fit.vutbr.cz>
2021-01-04 14:36:20 +0100

    update

--- mpi.h
+++ mpi.h
@@ -21,8 +21,8 @@ void mpi_clear(mpi_t rop);
 /* Assignment Functions */
 
 void mpi_set(mpi_t rop, const mpi_t op);
-void mpi_set_u64(mpi_t rop, uint64_t op);
 void mpi_set_u32(mpi_t rop, uint32_t op);
+void mpi_set_u64(mpi_t rop, uint64_t op);
 
 int mpi_set_str(mpi_t rop, const char *str, int base);
 
@@ -30,18 +30,18 @@ void mpi_swap(mpi_t rop1, mpi_t rop2);
 
 /* Conversion Functions */
 
-uint64_t mpi_get_u64(const mpi_t op);
 uint32_t mpi_get_u32(const mpi_t op);
+uint64_t mpi_get_u64(const mpi_t op);
 
 /* Arithmetic Functions */
 
 void mpi_add(mpi_t rop, const mpi_t op1, const mpi_t op2);
-void mpi_add_u64(mpi_t rop, const mpi_t op1, uint64_t op2);
 void mpi_add_u32(mpi_t rop, const mpi_t op1, uint32_t op2);
+void mpi_add_u64(mpi_t rop, const mpi_t op1, uint64_t op2);
 
 void mpi_sub(mpi_t rop, const mpi_t op1, const mpi_t op2);
-void mpi_sub_u64(mpi_t rop, const mpi_t op1, uint64_t op2);
 void mpi_sub_u32(mpi_t rop, const mpi_t op1, uint32_t op2);
+void mpi_sub_u64(mpi_t rop, const mpi_t op1, uint64_t op2);
 
 void mpi_mul(mpi_t rop, const mpi_t op1, const mpi_t op2);
 void mpi_mul_u32(mpi_t rop, const mpi_t op1, uint32_t op2);
